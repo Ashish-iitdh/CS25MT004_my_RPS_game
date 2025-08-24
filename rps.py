111619723b412188011756037fb15ceecfbe63ca
Ashish Saranshi <cs25mt004@iitdh.ac.in>
2025-08-24 22:49:11 +0530

    rules explanation Added

--- rps.py
+++ rps.py
@@ -4,4 +4,19 @@ def main():
 if __name__ == "__main__":
     main()
 
+def print_rules():
+    print("""
+Rules of Rock, Paper, Scissors:
+- Rock beats Scissors
+- Scissors beats Paper
+- Paper beats Rock
+- If both players choose the same, it’s a tie
+""")
+
+def main():
+    print("Welcome to Rock, Paper, Scissors!")
+    print_rules()
+
+if __name__ == "__main__":
+    main()
 
